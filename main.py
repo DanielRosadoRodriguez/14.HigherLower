29a6ad14bd770ac70db6da8ea710c4d89d3bc66f
Daniel Rosado <rosado.rodriguez.daniel93@gmail.com>
2022-11-01 09:57:34 -0600

    feat: changed spelling in print statements

--- main.py
+++ main.py
@@ -2,6 +2,6 @@ from domain import print_wellcome as pw, pick_person
 
 pw.print_wellcome()
 person1 = pick_person.pick_person()
-print(f"pesron1 {person1}")
+print(f"Option A: {person1.get('name')}, {person1.get('description')} from {person1.get('country')}")
 person2 = pick_person.pick_person()
-print(f"pesron2 {person2}")
+print(f"Option B: {person2.get('name')}, {person2.get('description')} from {person2.get('country')}")
