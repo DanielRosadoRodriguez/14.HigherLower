c206336a8017944018fb482d4c47839c7aa60fab
Daniel Rosado <rosado.rodriguez.daniel93@gmail.com>
2022-11-01 09:35:04 -0600

    feat: added option to print the persons chosen

--- main.py
+++ main.py
@@ -1,3 +1,7 @@
-from domain import print_wellcome as pw
+from domain import print_wellcome as pw, pick_person
 
 pw.print_wellcome()
+person1 = pick_person.pick_person()
+print(f"pesron1 {person1}")
+person2 = pick_person.pick_person()
+print(f"pesron2 {person2}")
